3f8760bcdb03ff1317848d117d9511448cbccaa4
gitbackspacer <49907563+gitbackspacer@users.noreply.github.com>
2022-11-22 14:24:46 +0000

    Update nodesdb.py
    added DB out file name option

--- lib/nodesdb.py
+++ lib/nodesdb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""usage: blobtools nodesdb             --nodes <NODES> --names <NAMES>
+"""usage: blobtools nodesdb             --nodes <NODES> --names <NAMES>   --nodesDB <DB>
                                         [-h|--help]
 
     Options:
